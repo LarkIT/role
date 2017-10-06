4ce1b558301b4353830b36f22983dcaf9a4afce9
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-06 12:22:42 -0600

    minor

--- manifests/railsapp.pp
+++ manifests/railsapp.pp
@@ -3,6 +3,6 @@ class role::railsapp {
   include profile::base
   include profile::railsapp
   include profile::monit
-  include profile::shibboleth
-  include profile::database::client
+#  include profile::shibboleth
+#  include profile::database::client
 }
