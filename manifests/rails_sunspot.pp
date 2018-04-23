9650ae5e414ca3b57b8a0624267dce66169b2842
Costa Arigirs <costa.argiris@lark-it.com>
2018-04-23 16:00:18 -0600

    Removed ordering dependency on rails_sunspot role
    DEVOPS-642

--- manifests/rails_sunspot.pp
+++ manifests/rails_sunspot.pp
@@ -6,5 +6,6 @@ class role::rails_sunspot inherits role::base {
   include ::profile::rails
   include ::profile::solr::client
   include ::profile::database::client
-  Class['::profile::base::repos']->  Class['::profile::database::client']
+  include ::profile::base::repos
+  include ::profile::database::client
 }
