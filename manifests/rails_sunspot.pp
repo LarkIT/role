b7e3f24e5a4bb79599c9c57525e2bd14019fd8a7
Costa Arigirs <costa.argiris@lark-it.com>
2018-04-23 16:15:13 -0600

    Commenting out repos base profile for rails_sunspot role
    DEVOPS-642

--- manifests/rails_sunspot.pp
+++ manifests/rails_sunspot.pp
@@ -6,6 +6,6 @@ class role::rails_sunspot inherits role::base {
   include ::profile::rails
   include ::profile::solr::client
   include ::profile::database::client
-  include ::profile::base::repos
+  #include ::profile::base::repos
   include ::profile::database::client
 }
